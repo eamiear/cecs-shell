d44258bc1707bfc6af02fb60a24f5a917ca2ca19
eamiear <justcall_me_k@163.com>
2019-12-20 17:38:01 +0800

    生成证书、端口开放

--- cecs.sh
+++ cecs.sh
@@ -1,16 +1,64 @@
 #!/bin/bash
 
 # B_PREFIX="/usr/local"
-B_PREFIX="/home/k"
+B_PREFIX="/usr/local"
 N_ENV="gcc gcc-c++ zlib zlib-devel pcre pcre-devel openssl openssl-devel"
 N_URL="http://nginx.org/download/nginx-1.15.9.tar.gz"
 N_FILE="nginx-1.15.9.tar.gz"
 N_DIR="nginx-1.15.9"
 N_PATH="nginx1159"
-N_PREFIX="/home/k/nginx1159"
+N_PREFIX="/usr/local/nginx1159"
+N_SSL="/usr/local/nginx1159/conf/ssl"
 PROJ_URL="https://github.com/ob-cloud/cecs/releases/download/cecs-v1.0.2/release.tar.gz"
 PROJ_FILE="release.tar.gz"
 PROJ="cecs"
+KEY="cecs"
+
+setFirewallPort() {
+  firewall-cmd --zone=public --add-port=443/tcp –permanent
+  systemctl restart firewalld.service
+}
+
+# generate ssl
+genSSL() {
+  if [ -d $N_SSL ];then
+    echo " SSL exist"
+  else
+    echo "[key] Create server key..."
+
+    # openssl genrsa -des3 -out $KEY.key 1024
+    openssl genrsa -out ssl.key 2048
+
+    echo "[csr] Create server certificate signing request..."
+
+    SUBJECT="/C=HK/ST=Hongkong/L=Hongkong/O=NASA/OU=DEV/CN=$KEY"
+
+    openssl req -new -key $KEY.key -out $KEY.csr -subj $SUBJECT
+
+    # echo "Remove password..."
+
+    # mv $KEY.key $KEY.origin.key
+    # openssl rsa -in $KEY.origin.key -out $KEY.key
+
+    echo "[ca] Sign SSL certificate..."
+    openssl req -new -x509 -key $KEY.key -out $KEY.ca.crt -days 3650 -subj $SUBJECT
+
+    echo "[crt] Sign SSL certificate..."
+
+    openssl x509 -req -days 3650 -in $KEY.csr -CA $KEY.ca.crt -CAkey $KEY.key -CAcreateserial -out $KEY.crt
+
+    echo "TODO:"
+    echo "Copy $KEY.crt to ../ssl/$KEY.crt"
+    echo "Copy $KEY.key to ../ssl/$KEY.key"
+    echo "Add configuration in nginx:"
+    echo "server {"
+    echo "    ..."
+    echo "    listen 443 ssl;"
+    echo "    ssl_certificate     ../ssl/$KEY.crt;"
+    echo "    ssl_certificate_key ../ssl/$KEY.key;"
+    echo "}"
+  fi
+}
 
 # Nginx base config
 setBaseNgCfg() {
@@ -34,8 +82,13 @@ setFeNgCfg() {
 echo "\033[32m setting $PROJ nginx config file \033[0m"
 cat >$N_PREFIX/conf/vconfigs/$PROJ.nginx.conf<<EOF
   server {
-    listen       80;
+    listen       443 ssl;
     server_name  localhost;
+
+    ssl_certificate ./ssl/$KEY.crt;
+    ssl_certificate_key ./ssl/$KEY.key;
+
+    charset utf-8;
     location /$PROJ {
         root  html;
         index  index.html index.htm;
@@ -46,7 +99,37 @@ EOF
 
 # Set Back end Nginx config
 setBeNgCfg() {
+echo "\033[32m setting $PROJ server nginx config file \033[0m"
+cat >$N_PREFIX/conf/vconfigs/$PROJ.server.nginx.conf<<EOF
+  server {
+       listen       443 ssl;
+       server_name  localhost;
 
+       ssl_certificate ./ssl/$KEY.crt;
+       ssl_certificate_key ./ssl/$KEY.key;
+
+       charset utf-8;
+       location / {
+            root   html;
+            index  index.html index.htm;
+            proxy_pass        https://zuul_server;
+			      proxy_set_header X-Real-IP $remote_addr;
+            proxy_set_header X-Forwarded-Proto $scheme;
+            proxy_set_header X-Forwarded-Port $Server_port;
+            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+            proxy_set_header Upgrade $http_upgrade;
+            proxy_set_header Connection "upgrade";
+            proxy_connect_timeout 60000;
+            proxy_read_timeout 60000;
+            proxy_send_timeout 60000;
+            proxy_intercept_errors off;
+        }
+        # location /cecs {
+        #   root html;
+        #   index index.html index.htm;
+        # }
+    }
+EOF
 }
 # Set Nginx Config file
 setConfig() {
@@ -63,6 +146,10 @@ setConfig() {
   tar -zxvf $PROJ_FILE # depress realease file
   rm -f $PROJ_FILE
   cd $N_PREFIX
+
+genSSL
+
+setFirewallPort
 
 setBaseNgCfg
 
